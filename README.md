f44283790bff87e97040c791bafe38033ca67a64
enggiqbal <enggiqbal@gmail.com>
2020-04-30 14:16:46 -0700

    update

--- README.md
+++ README.md
@@ -54,9 +54,72 @@ optional arguments:
 
 ```
 ## examples of command
+
+pass distance matrices
 ``` console
 python3 mpse.py -d MPSE/datasets/dataset_tabluar/data/dissimple1000_1.csv  MPSE/datasets/dataset_tabluar/data/dissimple1000_2.csv  MPSE/datasets/dataset_tabluar/data/dissimple1000_3.csv
+```
 
+run circlesquire example with 150 points, maximum iteration 100 then save output to mytest directory. 
+
+``` console
+python3.6 mpse.py -ds circlesquire -n 150 -max_iters 100 -e mytest
+
+
+iqbal@on-campus-10-138-77-23 MPSE-web % python3.6 mpse.py -ds circlesquire -n 10 -max_iters 100 -e mytest
+<h1>Please keep the window running</h1>
+Total Samples found:100<br>
+mpse.MPSE():
+multigraph.DISS():
+  nodes : 10
+  added attribute:
+    type : matrix
+    complete : True
+  added attribute:
+    type : matrix
+    complete : True
+  MPSE.initialize():
+    X0 : random
+    Q0 : given
+  dissimilarity stats:
+    number of views : 2
+    number of points : 10
+  embedding stats:
+    embedding dimension : 3
+    projection dimension : 2
+  MPSE.gd():
+    mpse method : fixed projections
+    initial stress : 1.89e+00
+gd.single(): 
+  computation parameters:
+    stochastic : True
+    constraint : False
+    scheme : mm
+    initial lr : 1
+    min_cost : 1.00e-03
+    max_iter : 100
+    max_step : 1.00e+10
+  progress:
+      99/100 : cost = 3.91e-03, grad = 1.38e-03, lr = 1.74e-01, step = 2.40e-04
+  results:
+    conclusion : maximum number of iterations reached
+    total iterations : 99
+    final cost : 3.91e-03
+    final gradient size : 1.38e-03
+    final learning rate : 1.74e-01
+    final step size : 2.40e-04
+    time : 1.33e+00 [sec]
+  Final stress : 2.68e-03
+Saving js data in: MPSE/outputs/mytest/coordinates.js
+JS file was saved in:  MPSE/outputs/mytest/coordinates.js
+DEPRECATION WARNING: The system version of Tk is deprecated and may be removed in a future release. Please don't rely on it. Set TK_SILENCE_DEPRECATION=1 to suppress this warning.
+<br> output path: MPSE/outputs/mytest/index.html
+<br><h1> <a target='_blank'  href ='static/mytest/index.html'>Interactive visualization</a></h1><br>
+<br><h2> <a target='_blank'  href ='static/mytest/mytest_pos.csv'>Output 3D position was saved here</a></h2><br>
+<br><h2> <a target='_blank'  href ='static/mytest/coordinates.js'>Output details (history, projections, position) was saved here</a></h2><br>
+<br>cost history saved as  MPSE/outputs/mytest/cost.png
+<br><img src=/static/mytest/cost.png>
+iqbal@on-campus-10-138-77-23 MPSE-web % 
 
 ```
 
--- mpse.py
+++ mpse.py
@@ -1,4 +1,4 @@
-# python3 mpse.py -d datasets/dataset_tabluar/data/dissimple1000_1.csv  datasets/dataset_tabluar/data/dissimple1000_2.csv -d3 datasets/dataset_tabluar/data/dissimple1000_3.csv
+# python3.6 mpse.py -ds 123
 
 import argparse
 import math
@@ -13,27 +13,37 @@ import shutil
 import random
 parser = argparse.ArgumentParser(description='MPSE')
 parser.add_argument('-d', '--d', type=argparse.FileType('r'), nargs='+',
-                    help='List of input files with distace matices', required=True)
+                    help='List of input files with distace matices', required=False)
 parser.add_argument('-o', '--output_dir', default='outputs',
                     help='Output directory', required=False)
 parser.add_argument('-e', '--experiment_name', default='exp',
                     help='Experiment name', required=False)
 parser.add_argument('-max_iters', '--max_iters', type=int,
                     default=1000, help='Max iterations', required=False)
+parser.add_argument('-esp', '--min_cost', type=int,
+                    default=0.001, help='stopping cost', required=False)
+
 parser.add_argument('-n', '--sample_size', type=int,
                     default=math.inf, help='Number of samples', required=False)
-parser.add_argument( '-X0','--X0', default=False, type=bool, choices=[True, False], help='Smart initialization',required=False)
+parser.add_argument('-X0', '--X0', default=False, type=bool,
+                    choices=[True, False], help='Smart initialization', required=False)
 parser.add_argument('-ps', '--projection_type',  default='standard', choices=['fixed',
-                    'same', 'standard', 'cylinder', 'orthogonal', 'normal', 'uniform', 'variable'], help="projection set", required=False)
+                                                                              'same', 'standard', 'cylinder', 'orthogonal', 'normal', 'uniform', 'variable'], help="projection set", required=False)
 parser.add_argument('-vt', '--visualization_template',  default='pointbased', choices=[
                     'pointbased', 'attributebased'], help="Visualization template", required=False)
 parser.add_argument('-an', '--average_neighbors', type=int,
                     default=32, help="average  neighbors", required=False)
-parser.add_argument('-ds', '--preloaded_dataset', default=None,  help="Preloaded Dataset", required=False)
-                    
+parser.add_argument('-ds', '--preloaded_dataset', default=None,
+                    help="Preloaded Dataset", required=False)
+
+
 args = parser.parse_args()
 
- 
+if args.preloaded_dataset == None and args.d == None:
+    print("Please provide either -d ( distace matices) or -ds (preloaded dataset)")
+    exit(1)
+
+
 def load123data(sample_size):
     path = 'MPSE/mview_examples/data/123/input/'
     Y1 = np.genfromtxt(path+'spicy_rice_10000_1.csv', delimiter=',')
@@ -41,33 +51,44 @@ def load123data(sample_size):
     Y3 = np.genfromtxt(path+'spicy_rice_10000_3.csv', delimiter=',')
     sample_size = min(len(Y1),  sample_size)
     sub = np.array(random.sample(range(len(Y1)), sample_size))
-    return [Y1[sub],Y2[sub],Y3[sub]]
+    return [Y1[sub], Y2[sub], Y3[sub]]
 
 
 print("<h1>Please keep the window running</h1>")
 
 
-
-if args.preloaded_dataset=='123':
-    D=load123data(args.sample_size)
-else:
+def load_data(args):
     D = [data.get_matrix(f) for f in args.d]
     print(f"Total Samples found:%d<br>" % len(D[0]))
     args.sample_size = min(len(D[0]), args.sample_size)
     sub = range(args.sample_size)
     D = [(a[sub])[:, sub] for a in D]
+    return D
 
+
+if args.preloaded_dataset == '123':
+    D = load123data(args.sample_size)
+
+if args.preloaded_dataset == 'credit':
+    args.d = ['MPSE/datasets/dataset_tabluar/data/dissimple1000_1.csv',
+              'MPSE/datasets/dataset_tabluar/data/dissimple1000_2.csv', 'MPSE/datasets/dataset_tabluar/data/dissimple1000_3.csv']
+    D = load_data(args)
+
+if args.preloaded_dataset == 'circlesquire':
+    args.d = ['MPSE/datasets/dataset_3D/circle_square_new/dist_circle.csv',
+              'MPSE/datasets/dataset_3D/circle_square_new/dist_square.csv']
+    D = load_data(args)
+
+if args.preloaded_dataset == None:
+    D = load_data(args)
 
 
 if args.projection_type == 'variable':
     args.projection_type = None
-if args.X0=='mds':
-    args.X0=True
-else:
-    args.X0=False
-    
- 
-mv = mview.basic(D,  Q=args.projection_type, verbose=2, smart_initialize=args.X0, max_iter=args.max_iters, average_neighbors=args.average_neighbors)
+
+
+mv = mview.basic(D,  Q=args.projection_type, verbose=2, smart_initialize=args.X0,
+                 max_iter=args.max_iters, average_neighbors=args.average_neighbors, min_cost=args.min_cost)
 
 projections = mv.Q
 costs = mv.H["costs"]
@@ -111,14 +132,17 @@ plt.plot(x, costs)
 costfile = os.path.join(args.output_dir, "cost.png")
 plt.savefig(costfile)
 sys.stdout.flush()
+print("<br> output path: "+ os.path.join(args.output_dir) + "index.html")
+
 print("<br><h1> <a target='_blank'  href ='static/" + args.experiment_name +
-      "/index.html'>Interactive visualization</a></h1><br>", flush=True) 
+      "/index.html'>Interactive visualization</a></h1><br>", flush=True)
 #print("<br>Output 3D position was saved in: ", posfile)
 
-print("<br><h2> <a target='_blank'  href ='static/" + args.experiment_name + "/"+ args.experiment_name + "_pos.csv"
-      "'>Output 3D position was saved here</a></h2><br>", flush=True) 
+print("<br><h2> <a target='_blank'  href ='static/" + args.experiment_name + "/" + args.experiment_name + "_pos.csv"
+      "'>Output 3D position was saved here</a></h2><br>", flush=True)
 
-print("<br><h2> <a target='_blank'  href ='static/" + args.experiment_name + "/coordinates.js'>Output details (history, projections, position) was saved here</a></h2><br>", flush=True) 
+print("<br><h2> <a target='_blank'  href ='static/" + args.experiment_name +
+      "/coordinates.js'>Output details (history, projections, position) was saved here</a></h2><br>", flush=True)
 
 
 print("<br>cost history saved as ", costfile)
