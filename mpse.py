f9d3caa7feeaf15ffee4f3952eda05c277fa1d69
enggiqbal <enggiqbal@gmail.com>
2020-05-04 16:22:41 -0700

    code refactoring

--- mpse.py
+++ mpse.py
@@ -2,8 +2,7 @@
 
 import argparse
 import math
-import pandas as pd 
-from MPSE.multiviewMDS import data
+import pandas as pd
 import numpy as np
 import os
 import sys
@@ -11,37 +10,7 @@ import MPSE.mview as mview
 import matplotlib.pyplot as plt
 import shutil
 import random
-parser = argparse.ArgumentParser(description='MPSE')
-parser.add_argument('-d', '--d', type=argparse.FileType('r'), nargs='+',
-                    help='List of input files with distace matices', required=False)
-parser.add_argument('-o', '--output_dir', default='outputs',
-                    help='Output directory', required=False)
-parser.add_argument('-e', '--experiment_name', default='exp',
-                    help='Experiment name', required=False)
-parser.add_argument('-max_iters', '--max_iters', type=int,
-                    default=1000, help='Max iterations', required=False)
-parser.add_argument('-esp', '--min_cost', type=int,
-                    default=0.001, help='stopping cost', required=False)
-
-parser.add_argument('-n', '--sample_size', type=int,
-                    default=math.inf, help='Number of samples', required=False)
-parser.add_argument('-X0', '--X0', default=False, type=bool,
-                    choices=[True, False], help='Smart initialization', required=False)
-parser.add_argument('-ps', '--projection_type',  default='standard', choices=['fixed',
-                                                                              'same', 'standard', 'cylinder', 'orthogonal', 'normal', 'uniform', 'variable'], help="projection set", required=False)
-parser.add_argument('-vt', '--visualization_template',  default='pointbased', choices=[
-                    'pointbased', 'attributebased'], help="Visualization template", required=False)
-parser.add_argument('-an', '--average_neighbors', type=int,
-                    default=32, help="average  neighbors", required=False)
-parser.add_argument('-ds', '--preloaded_dataset', default=None,
-                    help="Preloaded Dataset", required=False)
-
-
-args = parser.parse_args()
-
-if args.preloaded_dataset == None and args.d == None:
-    print("Please provide either -d ( distace matices) or -ds (preloaded dataset)")
-    exit(1)
+import time
 
 
 def load123data(sample_size):
@@ -54,9 +23,6 @@ def load123data(sample_size):
     return [Y1[sub], Y2[sub], Y3[sub]]
 
 
-print("<h1>Please keep the window running</h1>")
-
-
 def load_data(args):
     D = [pd.read_csv(f, header=None).values for f in args.d]
     print(f"Total Samples found:%d<br>" % len(D[0]))
@@ -66,88 +32,141 @@ def load_data(args):
     return D
 
 
-if args.preloaded_dataset == '123':
-    D = load123data(args.sample_size)
-
-if args.preloaded_dataset == 'credit':
-    path='MPSE/datasets/dataset_tabluar/data/'
-    args.d = [path+'dissimple1000_1.csv',  path+'dissimple1000_2.csv', path+'dissimple1000_3.csv']
-    D = load_data(args)
-
-if args.preloaded_dataset == 'circlesquire':
-    path='MPSE/datasets/dataset_3D/circle_square_new/'
-    args.d = [path+'dist_circle.csv', path+'dist_square.csv']
-    D = load_data(args)
-
-if args.preloaded_dataset == None:
-    D = load_data(args)
-
-
-
-args.projection_type = None if args.projection_type == 'variable' else args.projection_type 
-
-
-
-mv = mview.basic(D,  Q=args.projection_type, verbose=2, smart_initialize=args.X0,
-                 max_iter=args.max_iters, average_neighbors=args.average_neighbors, min_cost=args.min_cost)
-
-projections = mv.Q
-costs = mv.H["costs"]
-pos = mv.X
-
-# write to file
-args.output_dir = 'MPSE/outputs/' + args.experiment_name + "/"
-
-if not os.path.exists(args.output_dir):
-    os.makedirs(args.output_dir)
-path_to_copy = "cp -rf MPSE/resources/vistemplateattributebased/* "
-if args.visualization_template == "pointbased":
-    path_to_copy = "cp -rf MPSE/resources/vistemplatepointbased/* "
-
-os.system(path_to_copy + args.output_dir)
-
-f = open(args.output_dir+"/vis_param.js", "r")
-vis_param = f.read().replace("var numberofprojection=3;",
-                             "var numberofprojection=" + str(len(projections)) + ";")
-f.close()
-
-f = open(args.output_dir+"/vis_param.js", "w+")
-f.write(vis_param)
-f.close()
-
-js_file_path = os.path.join(args.output_dir, "coordinates.js")
-data.js_data_writer(pos, js_file_path, costs, projections)
-
-posfile = os.path.join(args.output_dir, args.experiment_name + "_pos.csv")
-np.savetxt(posfile, pos, delimiter=",")
-costfile = os.path.join(args.output_dir, args.experiment_name + "_costs.csv")
-np.savetxt(costfile, costs, delimiter=",")
+def js_data_writer(A, file_path, costs, P):
+    localtime = time.asctime(time.localtime(time.time()))
+    pos_tmp = A
+    jsdata = "var points=" + str(pos_tmp.tolist()) + ";"
+    f = open(file_path, "w")
+    f.write("var t='" + localtime + "';\n")
+    f.write("var steps={0};\n ".format(len(costs)))
+    costhistory = "\nvar costhistory=" + \
+        np.array2string(costs, precision=2, separator=',') + ";\n"
+    proj = "["
+    for x in P:
+        proj = proj + np.array2string(x, precision=6,
+                                      separator=',', suppress_small=True) + ","
+    proj = proj+"]"
+    f.write("var proj="+proj + ";\n")
+    f.write(jsdata)
+    f.write(costhistory)
+    f.close()
+   
 
 
-#posfile = os.path.join(args.output_dir, args.experiment_name + "_projections.csv")
-#np.savetxt(posfile, projections, delimiter=",")
+def get_matrix(args):
+    if args.preloaded_dataset == '123':
+        D = load123data(args.sample_size)
+
+    if args.preloaded_dataset == 'credit':
+        path = 'MPSE/datasets/dataset_tabluar/data/'
+        args.d = [path+'dissimple1000_1.csv',  path +
+                  'dissimple1000_2.csv', path+'dissimple1000_3.csv']
+        D = load_data(args)
+
+    if args.preloaded_dataset == 'circlesquire':
+        path = 'MPSE/datasets/dataset_3D/circle_square_new/'
+        args.d = [path+'dist_circle.csv', path+'dist_square.csv']
+        D = load_data(args)
+    if args.preloaded_dataset=='cluster':
+        path = 'MPSE/datasets/dataset_3D/clusters_dataset/'
+        args.d = [path+'dist_1.csv', path+'dist_2.csv', path+'dist_3.csv']
+        D = load_data(args)
+    if args.preloaded_dataset=='cupmouse':
+        path = 'MPSE/datasets/dataset_3D/cup_mouse/'
+        args.d = [path+'cup.csv', path+'mouse.csv' ]
+        D = load_data(args)
+    if args.preloaded_dataset == None:
+        D = load_data(args)
+    return D
 
 
-x = np.arange(len(costs))
-fig = plt.figure()
-ax = plt.axes()
-plt.plot(x, costs)
-costfile = os.path.join(args.output_dir, "cost.png")
-plt.savefig(costfile)
-sys.stdout.flush()
-print("<br> output path: " + os.path.join(args.output_dir) + "index.html")
+def write_output(mv, args):
+    projections = mv.Q
+    costs = mv.H["costs"]
+    pos = mv.X
+    args.output_dir = 'MPSE/outputs/' + args.experiment_name + "/"
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+    path_to_copy = "cp -rf MPSE/resources/vistemplateattributebased/* "
+    if args.visualization_template == "pointbased":
+        path_to_copy = "cp -rf MPSE/resources/vistemplatepointbased/* "
+    os.system(path_to_copy + args.output_dir)
+    f = open(args.output_dir+"/vis_param.js", "r")
+    vis_params = f.read().replace("var numberofprojection=3;",
+                                 "var numberofprojection=" + str(len(projections)) + ";")
+    vis_params=vis_params+" var backgroundcolor='"+  str(args.bgcolor) + "';\n " 
+    vis_params=vis_params+" var pointcolor='"+  str(args.pcolor) + "';\n " 
 
-print("<br><h1> <a target='_blank'  href ='static/" + args.experiment_name +
-      "/index.html'>Interactive visualization</a></h1><br>", flush=True)
-#print("<br>Output 3D position was saved in: ", posfile)
+    f.close()
+    f = open(args.output_dir+"/vis_param.js", "w+")
+    f.write(vis_params)
+    f.close()
+    js_file_path = os.path.join(args.output_dir, "coordinates.js")
+    js_data_writer(pos, js_file_path, costs, projections)
 
-print("<br><h2> <a target='_blank'  href ='static/" + args.experiment_name + "/" + args.experiment_name + "_pos.csv"
-      "'>Output 3D position was saved here</a></h2><br>", flush=True)
+    posfile = os.path.join(args.output_dir, args.experiment_name + "_pos.csv")
+    np.savetxt(posfile, pos, delimiter=",")
+    costfile = os.path.join(
+        args.output_dir, args.experiment_name + "_costs.csv")
+    np.savetxt(costfile, costs, delimiter=",")
 
-print("<br><h2> <a target='_blank'  href ='static/" + args.experiment_name +
-      "/coordinates.js'>Output details (history, projections, position) was saved here</a></h2><br>", flush=True)
+    fig = plt.figure()
+    ax = plt.axes()
+    plt.plot(np.arange(len(costs)), costs)
+    costfile = os.path.join(args.output_dir, "cost.png")
+    plt.savefig(costfile)
+    sys.stdout.flush()
+    print("<br> output path: " + os.path.join(args.output_dir) + "index.html")
+    print("<br><h1> <a target='_blank'  href ='static/" + args.experiment_name +
+          "/index.html'>Interactive visualization</a></h1><br>", flush=True)
+    print("<br><h2> <a target='_blank'  href ='static/" + args.experiment_name + "/" + args.experiment_name + "_pos.csv"
+          "'>Download 3D positions</a></h2><br>", flush=True)
+    print("<br><h2> <a target='_blank'  href ='static/" + args.experiment_name +
+          "/coordinates.js'>Output details (history, projections, position) was saved here</a></h2><br>", flush=True)
+ 
+    print("<br><img src=/static/"+args.experiment_name + "/cost.png" + ">")
 
 
-print("<br>cost history saved as ", costfile)
-print("<br><img src=/static/"+args.experiment_name + "/cost.png" + ">")
-# TODO: to be continued python3 mpse.py -d datasets/dataset_3D/circle_square_new/dist_circle.csv datasets/dataset_3D/circle_square_new/dist_circle.csv datasets/dataset_3D/circle_square_new/dist_circle.csv -p 2 -lr 0.001 -n 10 -alg classic
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MPSE')
+    parser.add_argument('-d', '--d', type=argparse.FileType('r'), nargs='+',
+                        help='List of input files with distace matices', required=False)
+    parser.add_argument('-o', '--output_dir', default='outputs',
+                        help='Output directory', required=False)
+    parser.add_argument('-e', '--experiment_name', default='exp',
+                        help='Experiment name', required=False)
+    parser.add_argument('-max_iters', '--max_iters', type=int,
+                        default=1000, help='Max iterations', required=False)
+    parser.add_argument('-esp', '--min_cost', type=int,
+                        default=0.001, help='stopping cost', required=False)
+    parser.add_argument('-n', '--sample_size', type=int,
+                        default=math.inf, help='Number of samples', required=False)
+    parser.add_argument('-X0', '--X0', default=False, type=bool,
+                        choices=[True, False], help='Smart initialization', required=False)
+    parser.add_argument('-ps', '--projection_type',  default='standard', choices=['fixed',
+                                                                                  'same', 'standard', 'cylinder', 'orthogonal', 'normal', 'uniform', 'variable'], help="projection set", required=False)
+    parser.add_argument('-vt', '--visualization_template',  default='pointbased', choices=[
+                        'pointbased', 'attributebased'], help="Visualization template", required=False)
+    parser.add_argument('-an', '--average_neighbors', type=int,
+                        default=32, help="average  neighbors", required=False)
+    parser.add_argument('-ds', '--preloaded_dataset', default=None,
+                        help="Preloaded Dataset", required=False)
+
+    parser.add_argument('-bgcolor', '--bgcolor', default='black',
+                            help="vis: background color", required=False)
+    parser.add_argument('-pcolor', '--pcolor', default='red',
+                            help="vis: poing color", required=False)
+                            
+    args = parser.parse_args()
+    if args.preloaded_dataset == None and args.d == None:
+        print("Please provide either -d ( distace matices) or -ds (preloaded dataset)")
+        exit(1)
+
+    print("<h1>Please keep the window running</h1>")
+
+    D = get_matrix(args)
+    args.projection_type = None if args.projection_type == 'variable' else args.projection_type
+
+    mv = mview.basic(D, Q=args.projection_type, verbose=2, smart_initialize=args.X0,
+                     max_iter=args.max_iters, average_neighbors=args.average_neighbors, min_cost=args.min_cost)
+    write_output(mv, args)
