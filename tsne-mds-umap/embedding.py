78c2bb9ae21636fb46bfd0fb07942f9c406040b6
enggiqbal <enggiqbal@gmail.com>
2020-06-01 14:41:16 -0700

    embedding with outputs

--- tsne-mds-umap/embedding.py
+++ tsne-mds-umap/embedding.py
@@ -15,17 +15,27 @@ import MPSE.mview as mview
 def load_data(n):
     (trainX, labels), _ = mnist.load_data()
     trainX=np.reshape(trainX,(trainX.shape[0], trainX.shape[1] * trainX.shape[2]))
+    
+     
+    index=[l in [2,3] for l in labels]
+    trainX=trainX[index]
+    labels=labels[index]
+
     return trainX[0:n,:], labels[0:n]
 
 def get_embedding(trainX,labels,fn,outfile):
     model = fn(n_components=2, random_state=0)
     pos = model.fit_transform(trainX)
+    
     x=pos.T[0]
     y=pos.T[1]
     x=x/max(max(x), abs(min(x)))
     y=y/max(max(y), abs(min(y)))
+    
     data = np.vstack((x,y, labels)).T
     df = pd.DataFrame(data=data, columns=("x", "y", "label"))
+    df[['x','y']].to_csv(outfile+'.csv')
+    df[['label']].to_csv("label.csv")
     draw_plot(df, 'x','y','label',outfile)
     return distance_matrix(df.values[:,0:2],df.values[:,0:2])
 
@@ -35,9 +45,17 @@ def draw_plot(data, x,y, l, outfile):
     sn.FacetGrid(df, hue=l,  size=8).map(plt.scatter, x, y,s= 20).add_legend()
     plt.savefig(outfile+".png")
 
-def get_MPSE(d1,d2,d3,labels):
-    mv = mview.basic([d1,d2,d3], Q="standard", average_neighbors=2,  max_iter=500, verbose=2)
-    data = np.vstack((mv.X.T, labels)).T
+def get_MPSE( labels):
+    labels=pd.read_csv("label.csv")['label'].values 
+    p=pd.read_csv("tsne.csv").values 
+    d1=distance_matrix(p,p)
+    p=pd.read_csv("umap.csv").values 
+    d2=distance_matrix(p,p)
+    # p=pd.read_csv("mds.csv").values
+    # d3=distance_matrix(p,p)
+ 
+    mv = mview.basic([d1,d2], Q="standard", average_neighbors=2,  max_iter=500, verbose=2)
+    data = np.vstack((mv.X.T, labels.T)).T
     df = pd.DataFrame(data=data, columns=("x", "y","z", "label"))
     draw_plot(df[["x","y","label"]], 'x','y','label',"1")
     draw_plot(df[["y","z","label"]], 'y','z','label',"2")
@@ -45,11 +63,11 @@ def get_MPSE(d1,d2,d3,labels):
     return mv.X
 
 if __name__ == "__main__":
-    trainX, labels=load_data(100)
+    trainX, labels=load_data(1000)
     tsne_dis=get_embedding(trainX,labels,TSNE,"tsne")
-    mds_dis=get_embedding(trainX,labels,MDS,"mds")
+    # #mds_dis=get_embedding(trainX,labels,MDS,"mds")
     umap_dis=get_embedding(trainX,labels,umap.UMAP,"umap")
 
-    mv=get_MPSE(tsne_dis,mds_dis,umap_dis,labels)
+    mv=get_MPSE(labels)
 
 
