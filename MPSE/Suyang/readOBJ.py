872d5c50118d022c3ae3c8e550e8b3e8643f6aba
Suyang Chen <hellochen19970916@gmail.com>
2021-03-25 04:49:00 -0700

    Update readOBJ.py

--- MPSE/Suyang/readOBJ.py
+++ MPSE/Suyang/readOBJ.py
@@ -2,12 +2,13 @@ from mpl_toolkits import mplot3d
 from matplotlib import pyplot as plt
 from stl import mesh
 import sys
+import trimesh
 import numpy  as np
 
 scaleX = 0
 scaleY = 0 
 scaleZ = 0
-SKIP = 100
+SKIP = 10
 
 def sample(matrix):
     temp = []
@@ -19,17 +20,16 @@ def sample(matrix):
 
 
 def readFile(filename):
+    mesh = trimesh.load(filename)
+    mesh.show()
+    points = mesh.sample(1024)
+
+    return np.array(points)
+
+def readFile_old(filename):
     temp = []
     with open(filename, 'r') as fd:
         fd.readline()
-        fd.readline()
-        fd.readline()
-        fd.readline()
-        line = fd.readline().strip().split()
-        fd.readline()
-        scaleX = float(line[2].split('=')[1])
-        scaleY = float(line[3].split('=')[1])
-        scaleZ = float(line[4].split('=')[1])
         for each in fd:
             each = each.strip().split()
             if len(each) < 4 or each[0] != 'v':
@@ -39,7 +39,10 @@ def readFile(filename):
             z = float(each[3])
             temp.append([x, y, z])
 
-    return np.array(temp)
+    
+    mesh = sample(np.array(temp))
+
+    return mesh
 
 def main():
     fileName = sys.argv[1]
@@ -52,10 +55,11 @@ def main():
     #your_mesh = mesh.Mesh.from_file(fileName)
     
     #axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
-    mesh = readFile(fileName)
-    mesh = sample(mesh)
+    points = readFile_old(fileName)
+    print(points)
+    #mesh = sample(mesh)
     with open(output+'.csv', 'w') as fd:
-        for each in mesh:
+        for each in points:
             axes.scatter(each[0], each[1], each[2], color= 'blue')
             fd.write(",".join([str(each[0]), str(each[1]), str(each[2])]) +"\n")
 
@@ -68,4 +72,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
